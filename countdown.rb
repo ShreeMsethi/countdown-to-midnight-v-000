aad3bb75dc7a730968fdcdc5d3a754d2f10b51af
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2019-02-19 00:54:07 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown (number)
   while number > 0
-  puts "#{number} SECONDS!"
+  puts "#{number} SECOND(S)!"
   number -= 1
   end
   "HAPPY NEW YEAR!"
@@ -13,7 +13,7 @@ countdown(5)
 
 def countdown_with_sleep(number)
   while number > 0
-  puts "#{number} SECONDS!"
+  puts "#{number} SECOND(S)!"
   sleep 1
   number -= 1
   end
